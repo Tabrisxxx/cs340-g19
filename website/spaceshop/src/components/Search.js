434669812504afab334ea3a0a6c46c902d8052f2
Timothy <wiliusat@oregonstate.edu>
2020-05-21 16:11:40 -0700

    search page 2

--- website/spaceshop/src/components/Search.js
+++ website/spaceshop/src/components/Search.js
@@ -3,7 +3,196 @@ import React from 'react';
 function Search() {
 	return (
 		<main>
-			<p>Search...</p>
+			<p>search</p>
+			<div class="ui internally celled grid">
+				<div class="row">
+					<div class="three wide column">
+						<img />
+					</div>
+					<div class="ten wide column">
+						<p></p>
+					</div>
+					<div class="three wide column">
+						<img />
+					</div>
+				</div>
+				<div class="row">
+					<div class="three wide column">
+						<img />
+					</div>
+					<div class="ten wide column">
+						<p></p>
+					</div>
+					<div class="three wide column">
+						<img />
+					</div>
+				</div>
+				<div class="row">
+					<div class="three wide column">
+						<img />
+					</div>
+					<div class="ten wide column">
+						<p></p>
+					</div>
+					<div class="three wide column">
+						<img />
+					</div>
+				</div>
+				<div class="row">
+					<div class="three wide column">
+						<img />
+					</div>
+					<div class="ten wide column">
+						<p></p>
+					</div>
+					<div class="three wide column">
+						<img />
+					</div>
+				</div>
+				<div class="row">
+					<div class="three wide column">
+						<img />
+					</div>
+					<div class="ten wide column">
+						<p></p>
+					</div>
+					<div class="three wide column">
+						<img />
+					</div>
+				</div>
+				<div class="row">
+					<div class="three wide column">
+						<img />
+					</div>
+					<div class="ten wide column">
+						<p></p>
+					</div>
+					<div class="three wide column">
+						<img />
+					</div>
+				</div>
+				<div class="row">
+					<div class="three wide column">
+						<img />
+					</div>
+					<div class="ten wide column">
+						<p></p>
+					</div>
+					<div class="three wide column">
+						<img />
+					</div>
+				</div>
+				<div class="row">
+					<div class="three wide column">
+						<img />
+					</div>
+					<div class="ten wide column">
+						<p></p>
+					</div>
+					<div class="three wide column">
+						<img />
+					</div>
+				</div>
+				<div class="row">
+					<div class="three wide column">
+						<img />
+					</div>
+					<div class="ten wide column">
+						<p></p>
+					</div>
+					<div class="three wide column">
+						<img />
+					</div>
+				</div>
+				<div class="row">
+					<div class="three wide column">
+						<img />
+					</div>
+					<div class="ten wide column">
+						<p></p>
+					</div>
+					<div class="three wide column">
+						<img />
+					</div>
+				</div>
+				<div class="row">
+					<div class="three wide column">
+						<img />
+					</div>
+					<div class="ten wide column">
+						<p></p>
+					</div>
+					<div class="three wide column">
+						<img />
+					</div>
+				</div>
+				<div class="row">
+					<div class="three wide column">
+						<img />
+					</div>
+					<div class="ten wide column">
+						<p></p>
+					</div>
+					<div class="three wide column">
+						<img />
+					</div>
+				</div>
+				<div class="row">
+					<div class="three wide column">
+						<img />
+					</div>
+					<div class="ten wide column">
+						<p></p>
+					</div>
+					<div class="three wide column">
+						<img />
+					</div>
+				</div>
+				<div class="row">
+					<div class="three wide column">
+						<img />
+					</div>
+					<div class="ten wide column">
+						<p></p>
+					</div>
+					<div class="three wide column">
+						<img />
+					</div>
+				</div>
+				<div class="row">
+					<div class="three wide column">
+						<img />
+					</div>
+					<div class="ten wide column">
+						<p></p>
+					</div>
+					<div class="three wide column">
+						<img />
+					</div>
+				</div>
+				<div class="row">
+					<div class="three wide column">
+						<img />
+					</div>
+					<div class="ten wide column">
+						<p></p>
+					</div>
+					<div class="three wide column">
+						<img />
+					</div>
+				</div>
+				<div class="row">
+					<div class="three wide column">
+						<img />
+					</div>
+					<div class="ten wide column">
+						<p></p>
+					</div>
+					<div class="three wide column">
+						<img />
+					</div>
+				</div>
+			</div>
 		</main>
 	);
 }
