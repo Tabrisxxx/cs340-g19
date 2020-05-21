8129b57fd7c00c3ace5db5b18be0adf394898a26
Timothy <wiliusat@oregonstate.edu>
2020-05-21 11:45:39 -0700

    login and signup

--- website/spaceshop/src/App.js
+++ website/spaceshop/src/App.js
@@ -7,14 +7,17 @@ import Home from './components/Home';
 import Profile from './components/Profile';
 import Browse from './components/Browse';
 import Orders from './components/Orders';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
 
 function App() {
 	return (
 		<Router>
 			<div>
 				<Navbar />
-
-				<Route path="/" exact component={Home} />
+				<Route path="/" exact component={Login} />
+				<Route path="/" exact component={SignUp} />
+				<Route path="/home" exact component={Home} />
 				<Route path="/browse" exact component={Browse} />
 				<Route path="/profile" exact component={Profile} />
 				<Route path="/orders" exact component={Orders} />
